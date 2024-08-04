e33d9171f78990bb065d6ea751b253c05427ae9a
Amir Alsayed <amyralsyd367@gmail.com>
2024-08-04 20:15:29 +0300

    adding mongodb client class

---
+++ utils/db.js
@@ -0,0 +1,36 @@
+// utils/db.js
+const { MongoClient } = require('mongodb');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    
+    const url = `mongodb://${host}:${port}`;
+    this.client = new MongoClient(url, { useUnifiedTopology: true });
+    this.db = this.client.db(database);
+
+    this.client.connect().catch((err) => {
+      console.error('Failed to connect to MongoDB:', err);
+    });
+  }
+
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    return this.db.collection('users').countDocuments();
+  }
+
+  async nbFiles() {
+    return this.db.collection('files').countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+module.exports = dbClient;
